9a0e747d60caeaa584b0822aafd315e2cb0938a2
Hannah E. <hannah.e.echo@gmail.com>
2025-03-02 16:21:19 -0700

    Project setup

---
+++ FetchRecipes_HEcho/FetchRecipes_HEcho/FetchRecipes_HEchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipes_HEchoApp.swift
+//  FetchRecipes_HEcho
+//
+//  Created by Hannah Echo on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipes_HEchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
