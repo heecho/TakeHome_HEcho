f0e17c52bc905ba4502541e9641721202120bb49
Hannah E. <hannah.e.echo@gmail.com>
2025-03-04 22:17:20 -0700

    add back uitests

---
+++ FetchRecipes_HEcho/FetchRecipes_HEchoUITests/FetchRecipes_HEchoUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  FetchRecipes_HEchoUITestsLaunchTests.swift
+//  FetchRecipes_HEchoUITests
+//
+//  Created by Hannah Echo on 3/2/25.
+//
+
+import XCTest
+@testable import FetchRecipes_HEcho
+
+final class FetchRecipes_HEchoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
